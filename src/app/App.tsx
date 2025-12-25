aa06784633789c0fb9ebabbcc2a666fff50ce9df
baonguyen <bao678393@gmail.com>
2025-12-25 10:17:18 +0700

    sua

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,18 @@
 import { useState } from 'react';
 import { LoginForm } from './components/LoginForm';
 import { RegisterForm } from './components/RegisterForm';
-import backgroundImage from "./img/img 2.png";
+import backgroundImage from "./img/imgbackground.png";
 
 export default function App() {
   const [showRegister, setShowRegister] = useState(false);
 
   return (
     <div className="min-h-screen w-full relative overflow-hidden">
-      {/* Background */}
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: `url(${backgroundImage})` }}
       />
 
-      {/* Content */}
       <div className="relative z-10 min-h-screen flex items-center justify-center p-4">
         {showRegister ? (
           <RegisterForm onSwitchToLogin={() => setShowRegister(false)} />
